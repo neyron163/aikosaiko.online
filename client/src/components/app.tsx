d113cbc6379ad20bf45ae6cbae84dc3a642a1aff
neyron <andrey163783@gmail.com>
2019-09-18 22:57:08 +0400

    grid

--- client/src/components/app.tsx
+++ client/src/components/app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
+import {Grid} from './grid/Grid';
 
 export const data = gql`
   query data {
@@ -13,7 +14,10 @@ export const App: React.FC = () => (
       {({ loading, data }: any) => {
         if (loading) return <div>...</div>
         return (
-          <div>{data.helloWorld}</div>
+          <div>
+            <Grid />
+            <div>{data.helloWorld}</div>
+          </div>
         )
       }
       }
