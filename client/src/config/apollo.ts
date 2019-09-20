a30e6e70bda62231a59200841faae4b6217f129a
neyron <andrey163783@gmail.com>
2019-09-20 16:08:01 +0400

    add cache for apollo

--- client/src/config/apollo.ts
+++ client/src/config/apollo.ts
@@ -1,5 +1,9 @@
 import ApolloClient from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const cache = new InMemoryCache();
 
 export default new ApolloClient({
   uri: "http://localhost:4000/graphql",
+  cache,
 });
