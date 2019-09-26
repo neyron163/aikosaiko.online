5ffeee303ad55e88e7c8e2be243652f326d05ca7
neyron <andrey163783@gmail.com>
2019-09-26 23:10:57 +0400

    add snapshots

---
+++ client/src/components/header/header.snap.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import {Header} from './header';
+
+jest.mock('../linkButton/linkButton', () => ({ LinkButton: 'LinkButton' }));
+
+describe('<Header />', () => {
+    it('should render correctly', () => {
+        const tree = renderer.create(<Header />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should render correctly if night true', () => {
+        const tree = renderer.create(<Header night />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
--- client/src/components/linkButton/linkButton.tsx
+++ client/src/components/linkButton/linkButton.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 export interface PropsType {
-    children: string;
     path: string;
 }
 
-export const LinkButton = ({children, path = '/'}: PropsType) => <Link to={path}>{children}</Link>;
+export const LinkButton: React.FC<PropsType> = ({children, path = '/'}) => <Link to={path}>{children}</Link>;
