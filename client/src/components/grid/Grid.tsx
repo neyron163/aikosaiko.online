2667227a96f3e02859d00e098d4ae30b90dc13e8
MAPersonGit <anon4math@gmail.com>
2019-09-27 11:26:19 +0300

    test for actions

--- client/src/components/grid/Grid.tsx
+++ client/src/components/grid/Grid.tsx
@@ -3,8 +3,8 @@ import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import classNames from "classnames";
 import s from "./grid.module.scss";
-import img from "../assets/img/800px.jpg";
 import emptySvg from "../assets/img/empty.svg";
+import { ItemType } from "./gridTypes";
 
 export const data = gql`
   query data {
@@ -25,23 +25,21 @@ export function Grid() {
     <Query query={data}>
       {({ loading, data }: any) => {
         if (loading) return null;
+        
         const { content } = data;
+
         return (
-          <div className={classNames(s.wrapper)}>
-            <aside className={classNames(s.leftBar)}></aside>
-            <div className={classNames(s.itemBox)}>
-              {content.map((itm: any) => (
+          <div className={s.wrapper}>
+            <aside className={s.leftBar}></aside>
+            <div className={s.itemBox}>
+              {content.map((itm: ItemType) => (
                 <div className={classNames(s.item, s.itemCard)} key={itm.id}>
-                  <figure className={classNames(s.itemFigure)}>
-                    <img src={img} alt="image" />
-                    <figcaption className={classNames(s.itemDescription)}>
-                      <div className={classNames(s.itemTopLine)}>
-                        <span
-                          className={classNames(s.itemNum)}
-                        >{`# ${itm.id}`}</span>
-                        <p className={classNames(s.itemName)}>
-                          {strFormatt(itm.title)}
-                        </p>
+                  <figure className={s.itemFigure}>
+                    <img src={itm.thumbnailUrl} alt="image" />
+                    <figcaption className={s.itemDescription}>
+                      <div className={s.itemTopLine}>
+                        <span className={s.itemNum}>{`# ${itm.id}`}</span>
+                        <p className={s.itemName}>{strFormatt(itm.title)}</p>
                         <p className={s.views}>Просмотров: 1000</p>
                         <footer className={s.itemFooter}>
                           <p className={s.svgBox}>
---
+++ client/src/components/grid/gridTypes.ts
@@ -0,0 +1,7 @@
+export type ItemType = {
+    albumId: string,
+    id: string,
+    title: string,
+    url: string,
+    thumbnailUrl: string,
+  };
